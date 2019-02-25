9e14fb276492d10c79bb1aaefaf53abd73819a51
Pavaris <pavarisket@gmail.com>
2019-02-25 18:27:57 -0500

    bar template

--- assets/dist/main.js
+++ assets/dist/main.js
@@ -118,7 +118,7 @@ $(document).ready(function () {
   /*
    * Mobile Navigation - Subnav
    ***************************/
-  if (isMobile) {
+  if ($(window).width() < 1030) {
     $('.nav-item.has-children > a').each(function () {
       mobileSubnav(this);
     });
@@ -134,8 +134,9 @@ $(document).ready(function () {
    ***************************/
   if ($('body').hasClass('current-home')) {
     console.log('home!');
-    $('body').scroll(function () {
-      let scrollTop = $('body').scrollTop();
+    $(window).scroll(function () {
+
+      let scrollTop = $(window).scrollTop();
       // console.log(scrollTop);
       if (scrollTop > 50) {
         $('#masthead').addClass('logo-show');
@@ -254,22 +255,10 @@ $(document).ready(function () {
 
   //specific AJAX calls.
 
-  //happenings page
   $(".eventsFilter").on('click', function () {
-    var all = false;
-    if ($(this).hasClass("active")) {
-      all = true;
-      $(".active").removeClass("active");
-    } else {
-      $(".active").removeClass("active");
-      $(this).addClass("active");
-    }
-
-    if (all == true) {
-      var filteredEvents = doAjax($(this).attr("box"), '');
-    } else {
-      var filteredEvents = doAjax($(this).attr("box"), $(this).attr("filter"));
-    }
+    $(".active").removeClass("active");
+    $(this).addClass("active");
+    var filteredEvents = doAjax($(this).attr("box"), $(this).attr("filter"));
     $(".all-events a").each(function (i, stuff) {
       var ele = $(this);
 
@@ -352,8 +341,9 @@ $(window).resize(function () {
   /*
    * Mobile Navigation ARIA
    ***************************/
-  if (isMobile) {
+  if (windowWidth < 1030) {
     // Overarching ARIA expanded
+    console.log('smaller menu!');
     $('#skip-to-navigation-link, .mobile-nav-header-close').attr('aria-expanded', isMenuOpen);
 
     // Subnav
@@ -483,6 +473,14 @@ function setCookie(cname, cvalue, exdays) {
   var expires = "expires=" + d.toUTCString();
   document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
+
+// function restClick(e){
+//   console.log('lcik!');
+//   if($(window).width() < 1030){
+//     e.preventDefault();
+//     $('.nav-children').slideToggle();
+//   }
+// }
 
 /***/ }),
 /* 2 */
